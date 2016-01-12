e1b3d16558db7324057abec1b5186ef0d2cd6371
Dan Chen <danielchen@Daniels-MacBook-Air.local>
2016-01-12 18:31:06 -0500

    started on orders route

---
+++ server/app/routes/orders/index.js
@@ -0,0 +1,11 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+router.route('/orders')
+    .get(function (req, res, next) {
+    })
+    .put(function (req, res, next) {
+    })
+    .post(function (req, res, next) {
+    })
