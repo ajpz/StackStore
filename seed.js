de1cf382cf55c313a6660aa8afd760555fbe565d
ajpz <perez.jonthan@gmail.com>
2016-01-12 16:30:52 -0500

    Still working on seed

--- seed.js
+++ seed.js
@@ -23,10 +23,16 @@ var chalk = require('chalk');
 var connectToDb = require('./server/db');
 var User = Promise.promisifyAll(mongoose.model('User'));
 var Car = Promise.promisifyAll(mongoose.model('Car'));
+var Category = Promise.promisifyAll(mongoose.model('Category'));
+var Order = Promise.promisifyAll(mongoose.model('Order'));
+var Address = Promise.promisifyAll(mongoose.model('Address'));
+var MakeAndModel = Promise.promisifyAll(mongoose.model('MakeAndModel'));
+var Review = Promise.promisifyAll(mongoose.model('Review'));
 
 // setting global references to mongoose database ids --once they have
 // been created.
 var address1, address2, category1, category2, make1, make2, model1, model2, reviews1, reviews2, car1, car2, user1, user2;
+
 
 var seedCategories = function() {
 
@@ -38,6 +44,8 @@ var seedCategories = function() {
             name: 'French Cars'
         }
     ]
+
+    return Categories.createAsync(categories);
 }
 
 var seedMakeAndModel = function() {
@@ -58,13 +66,13 @@ var seedCars = function() {
 
     var cars = [
         {
-            make: make1 // ID to MakeANdModel
+            make: make1, // ID to MakeANdModel
             model: 'Mustang',
             year: 1967,
             color: 'Red',
             condition: 'Excellent',
             mileage: 86000,
-            photos: ,
+            // photos: ,
             categoryIds: [category1],
             horsePower: 200,
             acceleration: 5.2,
@@ -79,7 +87,7 @@ var seedCars = function() {
             color: 'Black',
             condition: 'Good',
             mileage: 30000,
-            photos: ,
+            // photos: ,
             categoryIds: [category2],
             horsePower: 400,
             acceleration: 12,
@@ -125,22 +133,19 @@ var seedUsers = function() {
             google: 'googleAdmin',
             shippingAddress: address1 ,
             billingAddress: address2,
-            photos: ,
+            // photos: ,
             categories: category1,
-            orders: TODO,
             isAdmin: true,
             reviews: [reviews2]
         },
-            var users = [
         {
             email: 'user@gmail.com',
             password: 'user',
             google: 'googleUser',
             shippingAddress: address2,
             billingAddress: address1,
-            photos: ,
+            // photos: ,
             categories: category2,
-            orders: TODO,
             isAdmin: false,
             reviews: [reviews1]
         },
@@ -153,8 +158,27 @@ var seedUsers = function() {
             password: 'potus'
         }
     ]
+
+    return User.createAsync(users);
+}
+
+var seedOrders = function() {
+
+    var orders = [
+        {
+            status: 'Created',
+            userId: user1,
+            carId: car1,
+            amount: 93000
+        },
+        {
+            status: 'Processing',
+            userId: user2,
+            carId: car2,
+            orderDate: new Date(2015, 11, 23),
+            amount: 93000
+        }
     ]
-    return User.creaeAsync(users);
 }
 
 var seedReviews = function() {
@@ -162,13 +186,13 @@ var seedReviews = function() {
     var reviews = [
         {
             car: car1,
-            user: user1,
+            // user: user1,
             comment: 'This thing is a classic fantastic!',
             rating: 5
         },
         {
             car: car2,
-            user: user2,
+            // user: user2,
             comment: "Wow. Can't say that enough. I mean, wow!",
             rating: 5
         }
@@ -177,29 +201,24 @@ var seedReviews = function() {
 
 
 function seed () {
-    var docs = generateAll();
-    return Promise.map(docs, function (doc) {
-        return doc.save();
-    });
+    return seedCategories();
+    // var docs = generateAll();
+    // return Promise.map(docs, function (doc) {
+    //     return doc.save();
+    // });
 }
 
-db.drop = Promise.promisify(db.db.dropDatabase.bind(db.db));
+var db;
 
-
-connectToDb.then(function () {
-    db.drop()
-    .then(function(){
-
+    connectToDb.then(function(db){
+        console.log(db);
+        console.log(Object.keys(db));
+        db.drop = Promise.promisify(db.db.dropDatabase.bind(db.db));
     })
-
-
-    User.findAsync({}).then(function (users) {
-        if (users.length === 0) {
-            return seedUsers();
-        } else {
-            console.log(chalk.magenta('Seems to already be user data, exiting!'));
-            process.kill(0);
-        }
+    .then(function () {
+        db.drop()
+    .then(function(){
+        return seed();
     }).then(function () {
         console.log(chalk.green('Seed successful!'));
         process.kill(0);
