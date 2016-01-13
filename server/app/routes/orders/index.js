20748c3577297c8d2e7b38214c4cad43acbe3d08
Dan Chen <danielchen@Daniels-MacBook-Air.local>
2016-01-13 10:48:28 -0500

    orders get requested continue

--- server/app/routes/orders/index.js
+++ server/app/routes/orders/index.js
@@ -1,11 +1,17 @@
 'use strict';
 var router = require('express').Router();
+var Order = require("../../db/models/order.js");
 module.exports = router;
 
 router.route('/orders')
     .get(function (req, res, next) {
+        Order.find().exec()
+        .then(function (orders) {
+            res.status(200).json(orders);
+        });
     })
     .put(function (req, res, next) {
     })
     .post(function (req, res, next) {
     })
+
