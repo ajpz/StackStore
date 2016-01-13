75d68bf1d2fe0751ccc10ec0f837c2fd386c4ba5
Alan <ajohnson88@gmail.com>
2016-01-13 18:27:02 -0500

    changed directive to use isolated scope

--- browser/js/common/directives/sidebar/sidebar.js
+++ browser/js/common/directives/sidebar/sidebar.js
@@ -2,9 +2,15 @@ app.directive('sidebar', function(DataFactory) {
 
     return {
         restrict : 'E',
+        scope: {
+            categories: '='
+        },
         templateUrl : 'js/common/directives/sidebar/sidebar.html',
         link: function(scope, el, attrs) {
-            scope.categories = attrs.categories;
+            // DataFactory.fetchCategories()
+            //     .then(categories => {
+            //         scope.categories = categories;
+            //     });
         },
     }
 });
