1bb7e8af4163355688215d0bff3c070b73ad06a7
SHBailey <renfroswedding@gmail.com>
2016-01-14 12:21:05 -0500

    added dynamic photo and car name display to main page.

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -15,31 +15,7 @@ app.controller('HomeCtrl', function($scope, cars, DataFactory) {
 
     $scope.cars = cars;
 
-    var newCarDetails = {"make":"5696b37ac64dfed61c8ca3a8","model":"Mustang","year":1970,"color":"Black","condition":"Poor","mileage":86000,"horsePower":200,"acceleration":5.2,"kickassFactor":5,"price":93000,"__v":0,"count":1,"categoryIds":["5696b37ac64dfed61c8ca3a6"],"photos":[]}
 
-    var updateDetails = {"condition":"Good"};
-
-    $scope.addCar = function() {
-        DataFactory.addCar(newCarDetails)
-        .then(function(savedCar) {
-            console.log('the saved car is ', savedCar);
-            $scope.newCar = savedCar;
-        })
-    }
-
-    $scope.updateCar = function() {
-        DataFactory.updateCar($scope.newCar._id, updateDetails)
-        .then(function(updatedCar) {
-            $scope.updatedCar = updatedCar;
-        })
-    }
-
-    $scope.deleteCar = function() {
-        DataFactory.deleteCar($scope.newCar._id)
-        .then(function(deletedCar) {
-            $scope.deletedCar = deletedCar;
-        })
-    }
 })
 
 /*************************/
