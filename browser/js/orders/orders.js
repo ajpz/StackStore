214ff88f795b5c1e4b9c59ee2a1e414939ef47a9
SHBailey <renfroswedding@gmail.com>
2016-01-14 17:19:48 -0500

    fixed minor typos and started work on a view of the user's orders

---
+++ browser/js/orders/orders.js
@@ -0,0 +1,18 @@
+app.config(function($stateProvider) {
+    $stateProvider.state('orders', {
+        url: '/orders?userId',
+        controller: 'OrderCtrl',
+        templateUrl: 'js/orders/orders.html'
+    })
+
+})
+
+app.controller('OrderCtrl', function($scope, $stateParams, DataFactory) {
+    DataFactory.fetchOrdersForUser($stateParams.userId)
+    .then(function(orders){
+        $scope.orders = orders;
+    })
+
+
+})
+check
--- server/app/routes/cars/index.js
+++ server/app/routes/cars/index.js
@@ -24,6 +24,7 @@ router.route('/:carId')
         Car.findById(req.params.carId).exec()
         .then(function (car){
             if(!car) return res.status(404).end();
+            Car.populate('reviews')
             res.send(car)
         }).then(null, next)
     })
