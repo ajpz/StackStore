f2a222146b442ededd0df8803cea79e07277d6cb
ajpz <perez.jonthan@gmail.com>
2016-01-16 15:23:20 -0500

    Add resolve for car, inject CartFactory for listeners, add question on scope to pose to mark

--- browser/js/car-detail/car-detail.js
+++ browser/js/car-detail/car-detail.js
@@ -2,26 +2,31 @@ app.config(function($stateProvider) {
     $stateProvider.state('car', {
         url: '/cars/:carId',
         controller: 'CarCtrl',
-        templateUrl: 'js/car-detail/car-detail.html'
+        templateUrl: 'js/car-detail/car-detail.html',
+        resolve: {
+            car: function(DataFactory, $stateParams, CartFactory){
+                return DataFactory.fetchCar($stateParams.carId)
+            }
+        }
     })
-
 })
 
-app.controller('CarCtrl', function($scope, $stateParams, $state, DataFactory, CartFactory) {
+app.controller('CarCtrl', function($scope, car, CartFactory) {
 
-    $scope.cart;
+    //ASK MARK: scope of shopping-cart is inside the scope of this template.
+    //Does it make sense that the rendered cart borrows from shopping cart
+    //scope, before the addToCart method is ever invoked. This allows for cart
+    //to be populated early.
 
-    DataFactory.fetchCar($stateParams.carId)
-    .then(function(car){
-        $scope.car = car;
-    })
+    $scope.car = car;
 
     $scope.addToCart = function(car) {
-      CartFactory.addToCart(car)
-      .then(function(cart) {
-        $scope.cart = cart;
-        // $state.go('car');
-      })
+
+        CartFactory.addToCart(car)
+        .then(function(cart) {
+            $scope.cart = cart;
+        })
+
     }
 
 })
