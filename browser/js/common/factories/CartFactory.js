07988ec6435f05f52e0b043a7fabb60d90c4c219
ajpz <perez.jonthan@gmail.com>
2016-01-16 15:26:35 -0500

    Add local storage. Works now for basic case of auth/unauth user, merge carts, browser refresh, and add to cart.

--- browser/js/common/factories/CartFactory.js
+++ browser/js/common/factories/CartFactory.js
@@ -1,65 +1,74 @@
 app.factory('CartFactory', function($http, AuthService, $q, $rootScope, AUTH_EVENTS, DataFactory) {
 
+    //SUPER IMPORTANT: to have CartFactory actually
+    //register listeners on loginSuccess, CartFactory
+    //must be injected into a state. If not, it won't be
+    //loaded upon refresh, and the state won't ahve access
+    //to the refreshed cart. Better way to handle this?
 
-    // var self = this;
-    var currentUser = null;
-    var shoppingCart = { status: 'Created', car: [] };
+    var localStorageCart = JSON.parse(localStorage.getItem('visitingUserCart'));
+    var shoppingCart = localStorageCart ?
+            localStorageCart :
+            { status: 'Created', car: [] };
+
+    var saveShoppingCartToDb = function(cartToSave){
+        if(cartToSave._id) {
+            return DataFactory.updateOrder(cartToSave._id, cartToSave);
+        } else {
+            return DataFactory.addOrder(cartToSave);
+        }
+    };
 
     var refreshShoppingCart = function(user){
+        //gets user, gets shopping cart from db, merges local and db carts
+        //saves merged cart to db
         AuthService.getLoggedInUser()
         .then(function(user) {
-            currentUser = user;
+            shoppingCart.user = user._id;
             return DataFactory.fetchOrdersForUser(user._id, 'Created')
         })
-        .then(function(cart) {
-            if(cart.length > 0) shoppingCart = cart[0];
+        .then(function(databaseCartArr) {
+            var dbCart = databaseCartArr[0];
+            if(dbCart) {
+                dbCart.car = dbCart.car.concat(shoppingCart.car);
+                shoppingCart = dbCart;
+            }
+            return saveShoppingCartToDb(shoppingCart);
+        })
+        .then(function(databaseCart) {
+            shoppingCart = databaseCart;
+            localStorage.removeItem('visitingUserCart');
             $rootScope.$broadcast('LoadCart', shoppingCart);
-            return shoppingCart;
+            return;
         })
         .then(null, console.error.bind(console));
 
     };
 
     var destroyShoppingCart = function(){
-        currentUser = null;
         shoppingCart = { status: 'Created', car: [] };
+        $rootScope.$broadcast('LoadCart', shoppingCart);
     }
 
-    $rootScope.$on(AUTH_EVENTS.loginSuccess, function(){
-        refreshShoppingCart();
-    });
-    $rootScope.$on(AUTH_EVENTS.logoutSuccess, function() {
-        destroyShoppingCart();
-    });
+    $rootScope.$on(AUTH_EVENTS.loginSuccess, refreshShoppingCart);
+    $rootScope.$on(AUTH_EVENTS.logoutSuccess, destroyShoppingCart);
 
     return {
         getCurrentCart: function(){
             return shoppingCart; //synchronous!
         },
         addToCart: function(car){
-
             shoppingCart.car.push(car._id);
 
             if(AuthService.isAuthenticated()) {
-
-                var saveShoppingCartToDb = function(cartToSave){
-                    if(cartToSave._id) {
-                        return DataFactory.updateOrder(cartToSave._id, cartToSave);
-                    } else {
-                        return DataFactory.addOrder(cartToSave);
-                    }
-                };
-
                 return $q.when(saveShoppingCartToDb(shoppingCart))
                 .then(function(savedOrder){
-                    console.log("SAVED order: ",  savedOrder);
                     return shoppingCart = savedOrder;
                 })
 
             } else {
-                // save changes to local storage
-                console.log('order FACTORY: USER n/a AND SAVE');
-                return $q.when(order);
+                localStorage.setItem('visitingUserCart', JSON.stringify(shoppingCart));
+                return $q.when(shoppingCart);
             }
         },
         updateCart: function(){},
@@ -69,13 +78,6 @@ app.factory('CartFactory', function($http, AuthService, $q, $rootScope, AUTH_EVE
 
 
 /*
-UnAuth Users [local storage or session storage]
-
-
-
-
-Auth Users [user object on server has reference to order]
-
 
 Shared functionality
 
