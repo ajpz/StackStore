4d64e5816b830684f8179f04482d178f39dadd00
ajpz <perez.jonthan@gmail.com>
2016-01-18 10:54:21 -0500

    reorder requires for each of the models -- put makeAndModels first

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,13 +2,13 @@
 var router = require('express').Router();
 module.exports = router;
 
+router.use('/makes', require('./makeAndModels'));
 router.use('/members', require('./members'));
 router.use('/cars', require('./cars'));
 router.use('/orders', require('./orders'));
 router.use('/categories', require('./categories'));
 router.use('/users', require('./users'));
 router.use('/reviews', require('./reviews'));
-router.use('/makes', require('./makeAndModels'));
 
 // Make sure this is after all of
 // the registered routes!
