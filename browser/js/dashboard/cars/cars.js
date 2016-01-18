afee4a7d42a97941c2dec9029611e87432767cdf
Alan <ajohnson88@gmail.com>
2016-01-18 16:34:05 -0500

    add remove car functionality added

--- browser/js/dashboard/cars/cars.js
+++ browser/js/dashboard/cars/cars.js
@@ -1,10 +1,10 @@
-app.config( $stateProvider => {
+app.config($stateProvider => {
 
     $stateProvider.state('dashboard.cars', {
         url: '/cars',
         controller: 'CarsCtrl',
         templateUrl: 'js/dashboard/cars/cars.html',
-        resolve : {
+        resolve: {
             cars(DataFactory) {
                 return DataFactory.fetchCars();
             }
@@ -20,13 +20,12 @@ app.controller('CarsCtrl', ($scope, DataFactory, cars) => {
     $scope.newCar = {
         make: null,
         model: null,
-        year : null,
-        color : "Black",
-        condition : "Excellent",
-        mileage : null,
-        photos : [],
-        categories : [],
-        horespower : null,
+        year: null,
+        color: "Black",
+        condition: "Excellent",
+        mileage: null,
+        categories: [],
+        horespower: null,
         acceleration: null,
         price: null,
         count: null,
@@ -36,7 +35,7 @@ app.controller('CarsCtrl', ($scope, DataFactory, cars) => {
     $scope.removeCar = carId => {
         let len = $scope.cars.length;
         let indexOfSelected;
-        for (let i=0; i < len; i++) {
+        for (let i = 0; i < len; i++) {
             if ($scope.cars[i]._id === carId) {
                 indexOfSelected = i;
                 break;
@@ -55,5 +54,26 @@ app.controller('CarsCtrl', ($scope, DataFactory, cars) => {
         .then(categories => {
             $scope.categories = categories
         });
+
+    $scope.addCar = () => {
+        let car = {
+            make: (() => $scope.newCar.make._id)(),
+            model: $scope.newCar.model,
+            year: $scope.newCar.year,
+            color: $scope.newCar.color,
+            condition: $scope.newCar.condition,
+            mileage: $scope.newCar.mileage,
+            photos: (() => $scope.newCar.rawUrls.trim().split(","))(),
+            categories: (() => [$scope.newCar.categories._id])(),
+            horsepower: $scope.newCar.horsepower,
+            acceleration: $scope.newCar.acceleration,
+            price: $scope.newCar.price,
+            count: $scope.newCar.count,
+        }
+        DataFactory.addCar(car)
+            .then((car) => {
+                $scope.cars.push(car);
+            });
+    }
 
 })
